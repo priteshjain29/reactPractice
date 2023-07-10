f82e4ce1320da7e242e0ad624179a04665d18299
priteshjain29 <itspritesh2@gmail.com>
2023-07-10 18:03:56 +0530

    React Keeper App part 1

---
+++ src/components/footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Footer(){
+
+    const currentYear = new Date().getFullYear();
+    return (
+        <footer>
+        Copyright © {currentYear}
+        </footer> 
+    );
+         
+    
+}
+
+export default Footer;
